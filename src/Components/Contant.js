32db910231e81163129028ba809f987eeae229b2
md saif <mohdsaif9170613062@gmail.com>
2023-03-31 23:45:51 +0530

    initial commit

--- src/Components/Contant.js
+++ src/Components/Contant.js
@@ -1,24 +1,51 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Contant = () => {
-  const [name, setName] = useState("saif");
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "This is a Coco-Cola !",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "This is a Ice !",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "This is a Food !",
+    },
+  ]);
 
-  const handleNameChange = () => {
-    const names = ["Mohammad Saif", "Saqlain Raza", "Harish Zia"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
-  const handleClick = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    console.log(count);
-  };
+  // const [name, setName] = useState("saif");
+  // const [count, setCount] = useState(0);
 
-  const handleClick2 = () => {
-    console.log(count);
-  };
+  // const handleNameChange = () => {
+  //   const names = ["Mohammad Saif", "Saqlain Raza", "Harish Zia"];
+  //   const int = Math.floor(Math.random() * 3);
+  //   setName(names[int]);
+  // };
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  //   setCount(count + 1);
+  //   console.log(count);
+  // };
+
+  // const handleClick2 = () => {
+  //   console.log(count);
+  // };
 
   // const handleClick3 = (e) => {
   //   console.log(e);
@@ -26,11 +53,29 @@ const Contant = () => {
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {name} !</p>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
 
+      {/* <p onDoubleClick={handleClick}>Hello {name} !</p> 
       <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click it</button>
+      <button onClick={handleClick2}>Click it</button> */}
     </main>
   );
 };
