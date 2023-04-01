04338c4eacd16293cc30c31374167fb78348ab78
md saif <mohdsaif9170613062@gmail.com>
2023-04-01 16:33:53 +0530

    update components add list and delet

--- src/Components/Contant.js
+++ src/Components/Contant.js
@@ -28,6 +28,12 @@ const Contant = () => {
     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
+  const handleDelet = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   // const [name, setName] = useState("saif");
   // const [count, setCount] = useState(0);
 
@@ -53,24 +59,32 @@ const Contant = () => {
 
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            <label
-              style={item.checked ? { textDecoration: "line-through" } : null}
-              onDoubleClick={() => handleCheck(item.id)}
-            >
-              {item.item}
-            </label>
-            <FaTrashAlt role="button" tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelet(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}> Your List is Empty.</p>
+      )}
 
       {/* <p onDoubleClick={handleClick}>Hello {name} !</p> 
       <button onClick={handleNameChange}>Change Name</button>
