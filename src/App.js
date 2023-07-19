ac4fad83e2c9b5c3cb8db681757bcc6557140012
md saif <mohdsaif9170613062@gmail.com>
2023-07-19 21:56:09 +0530

    How to use useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from './Components/Header';
 import Contant from './Components/Contant';
 import Footer from './Components/Footer';
+import State from './Components/State';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Contant />
-      <Footer/>
+      {/* <Header /> */}
+      {/* <Contant /> */}
+      {/* <Footer /> */}
+      <State/>
     </div>
   );
 }
