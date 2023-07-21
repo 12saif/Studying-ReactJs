be4561cafef309cdde007e178bcb0b7bc2a15bf5
md saif <mohdsaif9170613062@gmail.com>
2023-07-21 22:34:12 +0530

    how to use useState increse and secres value

--- src/Components/State.js
+++ src/Components/State.js
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 
 const State = () => {
   const [myName, setMyName] = useState("Mohammad");
+  const [count, setCount] = useState(0);
+
   const changeName = () => {
     setMyName("Saif");
   };
 
   return (
-    <div>
-      <h1>{myName}</h1>
-      <button onClick={changeName}>Click Me</button>
-    </div>
+    <>
+      <div>
+        <h1>{myName}</h1>
+        <button onClick={changeName}>Click Me</button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increse</button>==
+        <button
+          onClick={() => (count === 0 ? setCount(0) : setCount(count - 1))}
+        >
+          Decrese
+        </button>
+      </div>
+    </>
   );
 };
 
